8bb364749db1a02a07ce984d187baf2454831994
Peuin <57762484+Peuin@users.noreply.github.com>
2020-10-21 11:24:52 +0700

    dá

--- dashboard/includes/connection.php
+++ dashboard/includes/connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect("localhost","root","","notes" ) or die ("error" . mysqli_error($conn));
+$conn = mysqli_connect("vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","kin0b0opn7p94i2q","ln7yknxknu79p6k4","x71oi8yxqyry6ju3" ) or die ("error" . mysqli_error($conn));
 ?>
